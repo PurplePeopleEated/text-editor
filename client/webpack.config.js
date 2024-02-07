34f834f97d0ec74a8ca8247322f9253519568dcc
Karra <karra.maynard@gmail.com>
2024-02-07 14:02:50 -0500

    add the css loaders and babel to rules module

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -45,8 +45,21 @@ module.exports = () => {
     ],
     module: {
       rules: [
-        
-      ],
-    },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader']
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        }
+      ]
+    }
   };
 };
